14a1c334cc092cf9aa3b5d9b916bff3d2db071bf
DennisFadeev <dennisfadeev@web.de>
2022-07-05 17:24:42 +0200

    added Favorite Table

--- src/main/java/edu/kit/tm/cm/smartcampus/buildingmanagement/logic/model/Favorite.java
+++ src/main/java/edu/kit/tm/cm/smartcampus/buildingmanagement/logic/model/Favorite.java
@@ -2,9 +2,43 @@ package edu.kit.tm.cm.smartcampus.buildingmanagement.logic.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "Favorite")
 @Data
-public class Favorite {
-  private String owner;
+public class
+Favorite {
+  @Id
+  @SequenceGenerator(
+          name = "favorite_sequence",
+          sequenceName = "favorite_sequence",
+          allocationSize = 1
+  )
+  @GeneratedValue(
+          strategy = SEQUENCE,
+          generator = "favorite_sequence"
+  )
+
+  @Column(
+          name= "identification_number",
+          updatable = false,
+          columnDefinition = "TEXT"
+
+  )
   private String identificationNumber;
+  @Column(
+          name = "owner",
+          nullable = false,
+          updatable = false,
+          columnDefinition = "TEXT"
+  )
+  private String owner;
+  @Column(
+          name = "reference_identification_number",
+          nullable = false,
+          columnDefinition = "TEXT"
+  )
   private String referenceIdentificationNumber;
 }
