4e45f2e559ee806443d3e52420373c5cdb831bf3
DennisFadeev <dennisfadeev@web.de>
2022-07-11 10:12:27 +0200

    changed Query in FavoriteRepository and changed extended Repo

--- src/main/java/edu/kit/tm/cm/smartcampus/buildingmanagement/infrastructure/database/FavoriteRepository.java
+++ src/main/java/edu/kit/tm/cm/smartcampus/buildingmanagement/infrastructure/database/FavoriteRepository.java
@@ -1,6 +1,7 @@
 package edu.kit.tm.cm.smartcampus.buildingmanagement.infrastructure.database;
 
 import edu.kit.tm.cm.smartcampus.buildingmanagement.logic.model.Favorite;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -8,8 +9,8 @@ import org.springframework.data.repository.query.Param;
 import java.util.Collection;
 
 /** This interface represents a favorite repository. */
-public interface FavoriteRepository extends CrudRepository<Favorite, String> {
+public interface FavoriteRepository extends JpaRepository<Favorite, String> {
 
-  @Query("SELECT '*' FROM favorite WHERE owner = 'owner'")
+  @Query("SELECT fav FROM Favorite fav WHERE fav.owner = 'owner'")
   Collection<Favorite> findByOwner(@Param("owner") String owner);
 }
