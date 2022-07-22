a1e1e1ceadc23c2ca0e9b6087e8f513f2fa11cb9
DennisFadeev <dennisfadeev@web.de>
2022-07-22 13:18:22 +0200

    added remaining get tests and new create tests

--- src/test/java/edu/kit/tm/cm/smartcampus/buildingmanagement/api/controller/ControllerTests.java
+++ src/test/java/edu/kit/tm/cm/smartcampus/buildingmanagement/api/controller/ControllerTests.java
@@ -5,9 +5,11 @@ import edu.kit.tm.cm.smartcampus.buildingmanagement.api.parser.GrpcObjectReader;
 import edu.kit.tm.cm.smartcampus.buildingmanagement.api.parser.GrpcObjectWriter;
 import edu.kit.tm.cm.smartcampus.buildingmanagement.infrastructure.service.BuildingManagementService;
 import edu.kit.tm.cm.smartcampus.buildingmanagement.logic.model.Building;
+import edu.kit.tm.cm.smartcampus.buildingmanagement.logic.model.Component;
+import edu.kit.tm.cm.smartcampus.buildingmanagement.logic.model.Room;
 import io.grpc.internal.testing.StreamRecorder;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -17,47 +19,51 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ControllerTests {
-    private ServerController controller;
 
     private GrpcObjectWriter writer = new GrpcObjectWriter();
 
-    private GrpcObjectReader reader = new GrpcObjectReader();
-    @BeforeEach
-    public void setup() {
+    private GrpcObjectReader reader = mock(GrpcObjectReader.class);
 
+    private BuildingManagementService service = mock(BuildingManagementService.class);
+
+    private ServerController controller = new ServerController(writer, reader, service);
+
+    GrpcBuilding grpcBuilding = GrpcBuilding.newBuilder()
+            .setCampusLocation(GrpcCampusLocation.SOUTH_CAMPUS)
+            .setBuildingName("")
+            .setBuildingNumber("")
+            .setLongitude(2.2)
+            .setLatitude(2.2)
+            .setIdentificationNumber("b-1")
+            .setNumFloors(0)
+            .build();
+
+    GrpcRoom grpcRoom = GrpcRoom.newBuilder()
+            .setRoomName("")
+            .setRoomType(GrpcRoomType.LECTURE_ROOM)
+            .setIdentificationNumber("r-1")
+            .setLatitude(2.2)
+            .setLatitude(2.2)
+            .setFloor(0)
+            .setRoomNumber("")
+            .setParentIdentificationNumber("")
+            .build();
+
+    GrpcComponent grpcComponent = GrpcComponent.newBuilder()
+            .setComponentDescription("")
+            .setComponentType(GrpcComponentType.STAIRS)
+            .setLatitude(2.2)
+            .setLongitude(2.2)
+            .setIdentificationNumber("c-1")
+            .setParentIdentificationNumber("")
+            .build();
+
+    @BeforeAll
+    public void setup() {
     }
+
     @Test
     public void testGetBuilding() throws Exception {
-        BuildingManagementService service = mock(BuildingManagementService.class);
-        when(service.getBuilding("b-1")).thenReturn(writeGetBuilding());
-        controller = new ServerController(writer, reader, service);
-        GetBuildingRequest request = GetBuildingRequest.newBuilder()
-                .setIdentificationNumber("b-1")
-                .build();
-        StreamRecorder<GetBuildingResponse> responseObserver = StreamRecorder.create();
-        controller.getBuilding(request, responseObserver);
-
-        assertTrue(responseObserver.awaitCompletion(5, TimeUnit.SECONDS));
-        assertNull(responseObserver.getError());
-        List<GetBuildingResponse> results = responseObserver.getValues();
-        assertEquals(1, results.size());
-        GetBuildingResponse response = results.get(0);
-        assertEquals(writeGetBuildingGrpc().getIdentificationNumber(), response.getBuilding().getIdentificationNumber());
-    }
-
-    public GrpcBuilding writeGetBuildingGrpc() {
-        return GrpcBuilding.newBuilder()
-                .setCampusLocation(GrpcCampusLocation.SOUTH_CAMPUS)
-                .setBuildingName("")
-                .setBuildingNumber("")
-                .setLongitude(2.2)
-                .setLatitude(2.2)
-                .setIdentificationNumber("b-1")
-                .setNumFloors(0)
-                .build();
-    }
-
-    public Building writeGetBuilding() {
         Building building = new Building();
         building.setBuildingName("");
         building.setCampusLocation(Building.CampusLocation.SOUTH_CAMPUS);
@@ -66,6 +72,140 @@ public class ControllerTests {
         building.setLatitude(2.2);
         building.setIdentificationNumber("b-1");
         building.setNumFloors(0);
-        return building;
+        when(service.getBuilding("b-1")).thenReturn(building);
+        GetBuildingRequest request = GetBuildingRequest.newBuilder()
+                .setIdentificationNumber("b-1")
+                .build();
+        StreamRecorder<GetBuildingResponse> responseObserver = StreamRecorder.create();
+        controller.getBuilding(request, responseObserver);
+        assertTrue(responseObserver.awaitCompletion(5, TimeUnit.SECONDS));
+        assertNull(responseObserver.getError());
+        List<GetBuildingResponse> results = responseObserver.getValues();
+        assertEquals(1, results.size());
+        GetBuildingResponse response = results.get(0);
+        assertEquals(grpcBuilding.getIdentificationNumber(), response.getBuilding().getIdentificationNumber());
+    }
+
+    @Test
+    public void testGetRoom() throws Exception {
+        Room room = new Room();
+        room.setRoomName("");
+        room.setType(Room.Type.LECTURE_ROOM);
+        room.setIdentificationNumber("r-1");
+        room.setLatitude(2.2);
+        room.setLongitude(2.2);
+        room.setFloor(0);
+        room.setRoomNumber("");
+        room.setParentIdentificationNumber("");
+        when(service.getRoom("r-1")).thenReturn(room);
+        GetRoomRequest request = GetRoomRequest.newBuilder()
+                .setIdentificationNumber("r-1")
+                .build();
+        StreamRecorder<GetRoomResponse> responseObserver = StreamRecorder.create();
+        controller.getRoom(request, responseObserver);
+        assertTrue(responseObserver.awaitCompletion(5, TimeUnit.SECONDS));
+        assertNull(responseObserver.getError());
+        List<GetRoomResponse> results = responseObserver.getValues();
+        assertEquals(1, results.size());
+        GetRoomResponse response = results.get(0);
+        assertEquals(grpcRoom.getIdentificationNumber(), response.getRoom().getIdentificationNumber());
+    }
+
+    @Test
+    public void testGetComponent() throws Exception {
+        Component component = new Component();
+        component.setComponentDescription("");
+        component.setType(Component.Type.STAIRS);
+        component.setLatitude(2.2);
+        component.setLongitude(2.2);
+        component.setIdentificationNumber("c-1");
+        component.setParentIdentificationNumber("");
+        when(service.getComponent("c-1")).thenReturn(component);
+        GetComponentRequest request = GetComponentRequest.newBuilder()
+                .setIdentificationNumber("c-1")
+                .build();
+        StreamRecorder<GetComponentResponse> responseObserver = StreamRecorder.create();
+        controller.getComponent(request, responseObserver);
+        assertTrue(responseObserver.awaitCompletion(5, TimeUnit.SECONDS));
+        assertNull(responseObserver.getError());
+        List<GetComponentResponse> results = responseObserver.getValues();
+        assertEquals(1, results.size());
+        GetComponentResponse response = results.get(0);
+        assertEquals(grpcComponent.getIdentificationNumber(), response.getComponent().getIdentificationNumber());
+    }
+
+    @Test
+    public void testCreateBuilding() throws Exception {
+        Building building = new Building();
+        building.setBuildingName("");
+        building.setCampusLocation(Building.CampusLocation.SOUTH_CAMPUS);
+        building.setBuildingNumber("");
+        building.setLongitude(2.2);
+        building.setLatitude(2.2);
+        building.setIdentificationNumber("b-1");
+        building.setNumFloors(0);
+        when(reader.read(grpcBuilding)).thenReturn(building);
+        when(service.createBuilding(building)).thenReturn(building);
+        CreateBuildingRequest request = CreateBuildingRequest.newBuilder()
+                .setBuilding(grpcBuilding)
+                .build();
+        StreamRecorder<CreateBuildingResponse> responseObserver = StreamRecorder.create();
+        controller.createBuilding(request, responseObserver);
+        assertTrue(responseObserver.awaitCompletion(5, TimeUnit.SECONDS));
+        assertNull(responseObserver.getError());
+        List<CreateBuildingResponse> results = responseObserver.getValues();
+        assertEquals(1, results.size());
+        CreateBuildingResponse response = results.get(0);
+        assertEquals(request.getBuilding().getIdentificationNumber(), response.getBuilding().getIdentificationNumber());
+    }
+
+    @Test
+    public void testCreateRoom() throws Exception {
+        Room room = new Room();
+        room.setRoomName("");
+        room.setType(Room.Type.LECTURE_ROOM);
+        room.setIdentificationNumber("r-1");
+        room.setLatitude(2.2);
+        room.setLongitude(2.2);
+        room.setFloor(0);
+        room.setRoomNumber("");
+        room.setParentIdentificationNumber("");
+        when(service.createRoom(room)).thenReturn(room);
+        when(reader.read(grpcRoom)).thenReturn(room);
+        CreateRoomRequest request = CreateRoomRequest.newBuilder()
+                .setRoom(grpcRoom)
+                .build();
+        StreamRecorder<CreateRoomResponse> responseObserver = StreamRecorder.create();
+        controller.createRoom(request, responseObserver);
+        assertTrue(responseObserver.awaitCompletion(5, TimeUnit.SECONDS));
+        assertNull(responseObserver.getError());
+        List<CreateRoomResponse> results = responseObserver.getValues();
+        assertEquals(1, results.size());
+        CreateRoomResponse response = results.get(0);
+        assertEquals(request.getRoom().getIdentificationNumber(), response.getRoom().getIdentificationNumber());
+    }
+
+    @Test
+    public void testCreateComponent() throws Exception {
+        Component component = new Component();
+        component.setComponentDescription("");
+        component.setType(Component.Type.STAIRS);
+        component.setLatitude(2.2);
+        component.setLongitude(2.2);
+        component.setIdentificationNumber("c-1");
+        component.setParentIdentificationNumber("");
+        when(service.createBuildingComponent(component)).thenReturn(component);
+        when(reader.read(grpcComponent)).thenReturn(component);
+        CreateComponentRequest request = CreateComponentRequest.newBuilder()
+                .setComponent(grpcComponent)
+                .build();
+        StreamRecorder<CreateComponentResponse> responseObserver = StreamRecorder.create();
+        controller.createBuildingComponent(request, responseObserver);
+        assertTrue(responseObserver.awaitCompletion(5, TimeUnit.SECONDS));
+        assertNull(responseObserver.getError());
+        List<CreateComponentResponse> results = responseObserver.getValues();
+        assertEquals(1, results.size());
+        CreateComponentResponse response = results.get(0);
+        assertEquals(request.getComponent().getIdentificationNumber(), response.getComponent().getIdentificationNumber());
     }
 }
